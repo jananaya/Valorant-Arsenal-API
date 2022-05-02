a106c9d3715923fca2ddc3837ad218de92011a69
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-02 17:09:01 -0500

    Create the ArsenalTest class

---
+++ src/test/java/domain/collections/ArsenalTest.java
@@ -0,0 +1,18 @@
+package domain.collections;
+
+public class ArsenalTest {	
+	@org.junit.jupiter.api.Test
+	void searchByName() {
+		
+	}
+	
+	@org.junit.jupiter.api.Test
+	void sort() {
+		
+	}
+	
+	@org.junit.jupiter.api.Test
+	void insert() {
+		
+	}
+}
