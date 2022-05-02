e95afb32648660a1aa36c3101a78bc570c6924bf
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-02 17:07:35 -0500

    Create the Arsenal class

---
+++ src/main/java/collections/Arsenal.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.ArrayList;
+
+import domain.interfaces.IArsenal;
+import domain.interfaces.IWeapon;
+import domain.types.Yardstick;
+
+public class Arsenal implements IArsenal {
+	@Override
+	public IWeapon searchByName(String name) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public ArrayList<IWeapon> sort(Yardstick yardstick) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public void insert(IWeapon weapon) {
+		// TODO		
+	}
+}
