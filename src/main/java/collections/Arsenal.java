9edc2c9d7c3f4e508454da25f2da1f9f85b9e1d1
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-04 18:18:12 -0500

    Implement searchByName method

--- src/main/java/collections/Arsenal.java
+++ src/main/java/collections/Arsenal.java
@@ -1,16 +1,43 @@
 package collections;
 
 import java.util.ArrayList;
+import types.BinaryTree;
 
 import domain.interfaces.IArsenal;
 import domain.interfaces.IWeapon;
 import domain.types.Yardstick;
 
+
 public class Arsenal implements IArsenal {
+	private BinaryTree<IWeapon> root;
+	private IWeapon weapon;
+	
+	public Arsenal() {
+		this.root = null;
+		this.weapon = null;
+	}
+	
 	@Override
 	public IWeapon searchByName(String name) {
-		// TODO
-		return null;
+		this.weapon = null;
+		
+		searchByName(name, this.root);
+		
+		return this.weapon;
+	}
+	
+	private void searchByName(String name, BinaryTree<IWeapon> r) {
+		if (r == null) {
+			return;
+		}
+		
+		if (r.getData().getName().equals(name)) {
+			this.weapon = r.getData();
+			return;
+		}
+		
+		searchByName(name, r.left());
+		searchByName(name, r.right());
 	}
 
 	@Override
