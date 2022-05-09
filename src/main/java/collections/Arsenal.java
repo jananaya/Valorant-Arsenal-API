8c0aef428c1fa17f6d723fb7469fe9ff5b7c2e4b
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-08 20:12:08 -0500

    Format Arsenal.java file

--- src/main/java/collections/Arsenal.java
+++ src/main/java/collections/Arsenal.java
@@ -1,6 +1,7 @@
 package collections;
 
 import java.util.ArrayList;
+
 import types.BinaryTree;
 
 import domain.interfaces.IArsenal;
@@ -8,123 +9,123 @@ import domain.interfaces.IWeapon;
 import domain.types.Yardstick;
 
 public class Arsenal implements IArsenal {
-	private BinaryTree<IWeapon> root;
+    private BinaryTree<IWeapon> root;
     private ArrayList<IWeapon> weapons;
-	private IWeapon weapon;
+    private IWeapon weapon;
 
-	public Arsenal() {
-		this.root = null;
-		this.weapon = null;
-		this.weapons = new ArrayList<>();
-	}
+    public Arsenal() {
+        this.root = null;
+        this.weapon = null;
+        this.weapons = new ArrayList<>();
+    }
 
-	@Override
-	public IWeapon searchByName(String name) {
-		this.weapon = null;
+    @Override
+    public IWeapon searchByName(String name) {
+        this.weapon = null;
 
-		searchByName(name, this.root);
+        searchByName(name, this.root);
 
-		return this.weapon;
-	}
+        return this.weapon;
+    }
 
-	private void searchByName(String name, BinaryTree<IWeapon> r) {
-		if (r == null) {
-			return;
-		}
-
-		if (r.getData().getName().equals(name)) {
-			this.weapon = r.getData();
-			return;
-		} else if (name.compareTo(r.getData().getName()) > 0) {
-			if (r.right() == null) {
-				return;
-			}
-			searchByName(name, r.right());
-		} else {
-			if (r.left() == null) {
-				return;
-			}
-			searchByName(name, r.left());
-		}
-	}
-
-	@Override
-	public ArrayList<IWeapon> searchByCategory(String category) {
-		if (empty())
-			return null;
-
-		this.weapons.clear();
-		searchByCategory(category, this.root);
-		return this.weapons;
-	}
-
-	private void searchByCategory(String category, BinaryTree<IWeapon> r) {
-		if (r == null)
-			return;
-
-		if (r.getData().getCategory().equals(category)) {
-			this.weapons.add(r.getData());
-		}
-
-		searchByCategory(category, r.left());
-		searchByCategory(category, r.right());
-	}
-
-        public ArrayList<IWeapon> getWeapons() {
-            if (empty())
-                return null;
-
-            this.weapons.clear();
-            getWeapons(this.root);
-
-            return this.weapons;
+    private void searchByName(String name, BinaryTree<IWeapon> r) {
+        if (r == null) {
+            return;
         }
 
-	private void getWeapons(BinaryTree<IWeapon> r) {
-            if (r == null)
+        if (r.getData().getName().equals(name)) {
+            this.weapon = r.getData();
+            return;
+        } else if (name.compareTo(r.getData().getName()) > 0) {
+            if (r.right() == null) {
                 return;
+            }
+            searchByName(name, r.right());
+        } else {
+            if (r.left() == null) {
+                return;
+            }
+            searchByName(name, r.left());
+        }
+    }
 
-            getWeapons(r.left());
+    @Override
+    public ArrayList<IWeapon> searchByCategory(String category) {
+        if (empty())
+            return null;
+
+        this.weapons.clear();
+        searchByCategory(category, this.root);
+        return this.weapons;
+    }
+
+    private void searchByCategory(String category, BinaryTree<IWeapon> r) {
+        if (r == null)
+            return;
+
+        if (r.getData().getCategory().equals(category)) {
             this.weapons.add(r.getData());
-            getWeapons(r.right());
         }
 
-	@Override
-	public ArrayList<IWeapon> sort(Yardstick yardstick) {
-		// TODO
-		return null;
-	}
+        searchByCategory(category, r.left());
+        searchByCategory(category, r.right());
+    }
 
-	@Override
-	public void insert(IWeapon weapon) {
-		if (empty()) {
-			this.root = new BinaryTree<IWeapon>(weapon);
-			return;
-		}
+    public ArrayList<IWeapon> getWeapons() {
+        if (empty())
+            return null;
 
-		insert(weapon, this.root);
-	}
+        this.weapons.clear();
+        getWeapons(this.root);
 
-	private void insert(IWeapon weapon, BinaryTree<IWeapon> r) {
-		if (weapon.getName().compareTo(r.getData().getName()) > 0) {
-			if (r.right() == null) {
-				r.linkRight(new BinaryTree<IWeapon>(weapon));
-				return;
-			}
+        return this.weapons;
+    }
 
-			insert(weapon, r.right());
-		} else {
-			if (r.left() == null) {
-				r.linkLeft(new BinaryTree<IWeapon>(weapon));
-				return;
-			}
+    private void getWeapons(BinaryTree<IWeapon> r) {
+        if (r == null)
+            return;
 
-			insert(weapon, r.left());
-		}
-	}
+        getWeapons(r.left());
+        this.weapons.add(r.getData());
+        getWeapons(r.right());
+    }
 
-	@Override
-	public boolean empty() {
-		return this.root == null;
-	}
+    @Override
+    public ArrayList<IWeapon> sort(Yardstick yardstick) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void insert(IWeapon weapon) {
+        if (empty()) {
+            this.root = new BinaryTree<IWeapon>(weapon);
+            return;
+        }
+
+        insert(weapon, this.root);
+    }
+
+    private void insert(IWeapon weapon, BinaryTree<IWeapon> r) {
+        if (weapon.getName().compareTo(r.getData().getName()) > 0) {
+            if (r.right() == null) {
+                r.linkRight(new BinaryTree<IWeapon>(weapon));
+                return;
+            }
+
+            insert(weapon, r.right());
+        } else {
+            if (r.left() == null) {
+                r.linkLeft(new BinaryTree<IWeapon>(weapon));
+                return;
+            }
+
+            insert(weapon, r.left());
+        }
+    }
+
+    @Override
+    public boolean empty() {
+        return this.root == null;
+    }
 }
