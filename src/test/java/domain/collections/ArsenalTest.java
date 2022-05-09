d2a759a298fd7d4ccf7a6929f301920a3fb019e5
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-08 20:07:49 -0500

    Write test for the new methods in Arsenal

--- src/test/java/domain/collections/ArsenalTest.java
+++ src/test/java/domain/collections/ArsenalTest.java
@@ -1,13 +1,13 @@
 package domain.collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import collections.Arsenal;
 import domain.entity.Weapon;
 import domain.types.Yardstick;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArsenalTest {
 	@org.junit.jupiter.api.Test
@@ -44,20 +44,23 @@ public class ArsenalTest {
 	@org.junit.jupiter.api.Test
 	void sort() {
 		ArrayList<String> specs = new ArrayList<>();
-		Weapon w1 = new Weapon("Desert Eagle", "Handgun", specs, "", 0, 0, 0);
+		Weapon w1 = new Weapon("Desert Eagle", "Handgun", specs, "", 3, 1, 9);
 
 		specs.add("This is a spec!");
 
-		Weapon w2 = new Weapon("AK47", "Fusil", specs, "", 0, 0, 0);
+		Weapon w2 = new Weapon("AK47", "Fusil", specs, "", 9, 5, 15);
 
 		specs.add("This is another spec!");
 
-		Weapon w3 = new Weapon("Knife", "Melee", specs, "", 0, 0, 0);
+		Weapon w3 = new Weapon("Knife", "Melee", specs, "", 2, 4, 20);
 
 		Arsenal arsenal = new Arsenal();
 		ArrayList<Weapon> weaponsByName = new ArrayList<>(Arrays.asList(w2, w1, w3));
 		ArrayList<Weapon> weaponsByCategory = new ArrayList<>(Arrays.asList(w2, w1, w3));
 		ArrayList<Weapon> weaponsByNumberOfSpecs = new ArrayList<>(Arrays.asList(w3, w2, w1));
+		ArrayList<Weapon> weaponsByRateOfFire = new ArrayList<>(Arrays.asList(w3, w1, w2));
+		ArrayList<Weapon> weaponsByDamage = new ArrayList<>(Arrays.asList(w1, w3, w2));
+		ArrayList<Weapon> weaponsByRating = new ArrayList<>(Arrays.asList(w1, w2, w3));
 
 		arsenal.insert(w1);
 		arsenal.insert(w2);
@@ -66,5 +69,34 @@ public class ArsenalTest {
 		assertEquals(weaponsByName, arsenal.sort(Yardstick.NAME));
 		assertEquals(weaponsByCategory, arsenal.sort(Yardstick.CATEGORY));
 		assertEquals(weaponsByNumberOfSpecs, arsenal.sort(Yardstick.NUMBER_OF_SPECS));
+		assertEquals(weaponsByRateOfFire, arsenal.sort(Yardstick.RATE_OF_FIRE));
+		assertEquals(weaponsByDamage, arsenal.sort(Yardstick.DAMAGE));
+		assertEquals(weaponsByRating, arsenal.sort(Yardstick.RATING));
+	}
+
+	@org.junit.jupiter.api.Test
+	void searchByCategory() {
+		Arsenal arsenal = new Arsenal();
+
+		Weapon w1 = new Weapon("Handgun weapon", "Handgun", null, "", 0, 0, 0);
+		Weapon w2 = new Weapon("Another handgun weapon", "Handgun", null, "", 0, 0, 0);
+		Weapon w3 = new Weapon("Knife", "Melee", null, "", 0, 0, 0);
+
+		ArrayList<Weapon> handguns = new ArrayList<>(Arrays.asList(w1, w2));
+
+		arsenal.insert(w1);
+		arsenal.insert(w2);
+		arsenal.insert(w3);
+
+		assertEquals(handguns, arsenal.searchByCategory("Handgun"));
+	}
+
+	@org.junit.jupiter.api.Test
+	void empty() {
+		Arsenal arsenal = new Arsenal();
+
+		assertTrue(arsenal.empty());
+		arsenal.insert(new Weapon("", "", null, "", 0, 0, 0));
+		assertFalse(arsenal.empty());
 	}
 }
