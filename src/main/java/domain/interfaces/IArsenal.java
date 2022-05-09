d31115e4bc9ae376f150c9f4919c1ac79fe60970
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-08 20:05:29 -0500

    Add more methods to Arsenal interface

--- src/main/java/domain/interfaces/IArsenal.java
+++ src/main/java/domain/interfaces/IArsenal.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 
 public interface IArsenal {
     IWeapon searchByName(String name);
+    ArrayList<IWeapon> searchByCategory(String category);
     ArrayList<IWeapon> sort(Yardstick yardstick);
     void insert(IWeapon weapon);
     ArrayList<IWeapon> getWeapons();
+    boolean empty();
 }
