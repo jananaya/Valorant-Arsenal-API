0be377b93bc9558e8738596473a408b9f538de73
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-10 03:42:25 -0500

    Add Weapon rest controller

---
+++ src/main/java/valorant/api/controllers/WeaponController.java
@@ -0,0 +1,59 @@
+package valorant.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+import types.Yardstick;
+import java.util.ArrayList;
+import model.ModelWeapon;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import schema.repository.WeaponRepository;
+import schema.WeaponDTO;
+import schema.WeaponMapper;
+
+@RestController
+public class WeaponController {
+
+    private final ModelWeapon model;
+    private Map sortingYardstick;
+    
+    public WeaponController() {
+        this.model = new ModelWeapon(new WeaponRepository());
+        this.sortingYardstick = new HashMap();
+        
+        sortingYardstick.put("specs", Yardstick.NUMBER_OF_SPECS);
+        sortingYardstick.put("category", Yardstick.CATEGORY);
+        sortingYardstick.put("damage", Yardstick.DAMAGE);
+        sortingYardstick.put("rateOfFire", Yardstick.RATE_OF_FIRE);
+        sortingYardstick.put("rating", Yardstick.RATING);
+    }
+    
+    @GetMapping("/arsenal/{name}")
+    public WeaponDTO searchWeapon(@PathVariable("name") String name) {
+        return WeaponMapper.toDto(
+            this.model.searchByName(name)
+        );
+    }
+    
+    @GetMapping("/arsenal")
+    public ArrayList<WeaponDTO> sendWeapons(@RequestParam(value="sort", defaultValue = "") String sort) {
+        if (this.sortingYardstick.containsKey(sort)) {
+            return WeaponMapper.toDto(
+                    this.model.sort((Yardstick)this.sortingYardstick.get(sort))
+            );
+        }
+        
+        return WeaponMapper.toDto(
+                this.model.getWeapons()
+        );
+    }
+    
+    @GetMapping("/arsenal/category/{category}")
+    public ArrayList<WeaponDTO> sendWeaponsFromCategory(@PathVariable("category") String category) {
+        return WeaponMapper.toDto(
+            this.model.searchByCategory(category)
+        );
+    }
+}
