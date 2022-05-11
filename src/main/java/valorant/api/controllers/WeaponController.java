1940e76917de77fbb44690d7e3445393c382a8db
jananaya <jancarlosanayajimenez@gmail.com>
2022-05-11 08:36:45 -0500

    Use CORS annotation on the Weapon controller

--- src/main/java/valorant/api/controllers/WeaponController.java
+++ src/main/java/valorant/api/controllers/WeaponController.java
@@ -7,17 +7,19 @@ import java.util.*;
 import types.Yardstick;
 import java.util.ArrayList;
 import model.ModelWeapon;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import schema.repository.WeaponRepository;
 import schema.WeaponDTO;
 import schema.WeaponMapper;
 
+@CrossOrigin
 @RestController
 public class WeaponController {
 
     private final ModelWeapon model;
-    private Map sortingYardstick;
+    private final Map sortingYardstick;
     
     public WeaponController() {
         this.model = new ModelWeapon(new WeaponRepository());
